58b4fcfe6514ef31ae750bcb72de7c4bcf75a99b
David Rodriguez <davidmauricio.rodriguez040@comunidadunir.net>
2025-03-11 17:09:39 -0500

    code/servicios

---
+++ servicios/config/config.php
@@ -0,0 +1,15 @@
+<?php
+class classConfig{
+
+	public function vecData(){
+		$db = [
+			'host' => '*********',
+			'username' => '***********',
+			'password' => '*********',
+			'db' => '*********'
+		];
+		return $db;
+	}
+	
+}
+?>
