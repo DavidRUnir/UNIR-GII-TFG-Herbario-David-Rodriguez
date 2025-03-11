f5dd0bcfbfef12cc6614b621f7f2b90c721837ca
David Rodriguez <davidmauricio.rodriguez040@comunidadunir.net>
2025-03-11 17:08:29 -0500

    code/config

---
+++ config/frmConfig.php
@@ -0,0 +1,16 @@
+<?php
+class classConfig{
+	public function rutaSelect(){
+		//return ("http://localhost/proyectos/9mecanicaadmin/servicios/rutas/rutaSelect.php/?");
+		return ("https://herbario.espoch.edu.ec/servicios/rutas/rutaSelect.php/?");
+	}
+	public function rutaInsert(){
+		//return ("http://localhost/proyectos/9mecanicaadmin/servicios/rutas/rutaInsert.php/?");
+		return ("https://herbario.espoch.edu.ec/servicios/rutas/rutaEditar.php/?");
+	}
+	public function rutaEditar(){
+		//return ("http://localhost/proyectos/9mecanicaadmin/servicios/rutas/rutaEditar.php/?");
+		return ("https://herbario.espoch.edu.ec/servicios/rutas/rutaEditar.php/?");
+	}
+}
+?>
