0b89a6700a7ae53ac0a872b4e3e143a5a1f7b52e
David Rodriguez <davidmauricio.rodriguez040@comunidadunir.net>
2025-03-11 17:07:00 -0500

    Delete servicios directory

--- servicios/rutas/rutaEditar.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("../modelo/modEditar.php");
-$ruta = new classEditar();
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-	if (isset($_GET['opc']) && $_GET['opc'] == "modHerbario")
-		$ruta -> editarHerbario($_POST);
-}
-?>
